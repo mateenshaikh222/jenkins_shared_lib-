2c42d54e9e8fb3fa16d425d4d70ed63e59d3154c
mateenshaikh222 <139356532+mateenshaikh222@users.noreply.github.com>
2023-09-13 15:00:43 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId){
 
-    withSonarQubeEnv(credentialsId: SonarQube_API) {
+    withSonarQubeEnv(credentialsId: credentialsId) {
          sh 'mvn clean package sonar:sonar'
     }
 }
